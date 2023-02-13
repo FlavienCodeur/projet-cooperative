21bf8eae23d3a99439620794632039d2a45ec362
FlavienCodeur <flavienrousse2003@gmail.com>
2023-02-13 16:49:16 +0100

    remote database and resolve null object , and remove and add mgrations

--- appflux/admin.py
+++ appflux/admin.py
@@ -3,8 +3,12 @@ from appflux.models import Entrepreneur
 from django.contrib.auth.admin import UserAdmin
 
 class EntrepreneurAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nom', 'prenom')
+    list_display = ('id', 'nom', 'prenom', 'email')
     ordering = ('id',)
+    search_fields = ('nom', 'prenom', 'email')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = () 
 
 
 admin.site.register(Entrepreneur, EntrepreneurAdmin)
--- appflux/models.py
+++ appflux/models.py
@@ -16,21 +16,21 @@ STRUCTURE = (
 
 
 class Entrepreneur(models.Model):
-    nom = models.CharField (max_length=30)
-    prenom = models.CharField(max_length=30)
+    nom = models.CharField (max_length=30,)
+    prenom = models.CharField(max_length=30,)
     matricule = models.CharField(max_length=30, blank=True)
     civilite = models.CharField(max_length=20, choices=ROLES)
-    photo = models.ImageField(null=True, blank=True)
-    telephone = models.IntegerField(blank=True, null=True)
-    structure = models.CharField(max_length=40, choices=STRUCTURE, blank=True, null=True)
-    email = models.CharField(max_length=150)
-    nationalite = models.CharField(max_length=150, blank=True, null=True)
-    naissance = models.CharField(max_length=100, verbose_name="pays de naissance", null=True, blank=True)
-    numero = models.IntegerField(blank=True, null=True, verbose_name="numero de telephone professionel")
-    adresse_postal = models.CharField(blank=True, null=True, max_length=200)
-    securite_social = models.CharField(blank=True, null=True, max_length=240)
-    date_naissance = models.DateField(blank=True, null=True)
-    ville = models.CharField(max_length=120, verbose_name="lieu de residence", blank=True, null=True)
+    photo = models.ImageField(default='avatar.jpg', upload_to='entrepreneur_avatars', blank=True,)
+    telephone = models.CharField(max_length=10 ,blank=True, )
+    structure = models.CharField(max_length=40, choices=STRUCTURE,)
+    email = models.CharField(max_length=150,)
+    nationalite = models.CharField(max_length=150,)
+    naissance = models.CharField(max_length=100, verbose_name="pays de naissance",)
+    numero = models.CharField( max_length=10 ,verbose_name="numero de telephone professionel", blank=True)
+    adresse_postal = models.CharField( max_length=200)
+    securite_social = models.CharField(max_length=240, blank=True,)
+    date_naissance = models.DateField()
+    ville = models.CharField(max_length=120, verbose_name="lieu de residence", blank=True )
 
     def __str__(self):
         return f'{self.nom} nom'
